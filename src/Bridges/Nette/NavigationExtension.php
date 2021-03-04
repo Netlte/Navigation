c7d17ff7360ac9858d859b11bea9d98e8d56dea3
Tomáš Holan <tomas@holan.dev>
2021-03-04 18:17:07 +0100

    Removed unused config schema

--- src/Bridges/Nette/NavigationExtension.php
+++ src/Bridges/Nette/NavigationExtension.php
@@ -4,44 +4,10 @@ namespace Netlte\Navigation\Bridges\Nette;
 
 use Netlte\Navigation\Manager;
 use \Nette\DI\CompilerExtension;
-use Nette\Schema\Expect;
-use Nette\Schema\Schema;
 
 class NavigationExtension extends CompilerExtension {
 
-	public function getConfigSchema(): Schema {
-		$items = [
-			'caption' => Expect::string()->required(),
-			'link'    => Expect::string()->nullable()->required(false),
-			'url'     => Expect::bool()->required(false),
-			'label'   => Expect::structure(
-				[
-					'caption' => Expect::string()->required(),
-					'color'   => Expect::string()->nullable()->required(false),
-				]
-			)->required(false),
-			'icon'    => Expect::structure(
-				[
-					'icon'  => Expect::string()->required(),
-					'color' => Expect::string()->nullable()->required(false),
-				]
-			)->required(false),
-		];
-
-		$items['items'] = Expect::arrayOf(Expect::structure($items));
-
-		$sections = Expect::structure(
-			[
-				'caption' => Expect::string()->nullable()->required(false),
-				'items'   => Expect::arrayOf(Expect::structure($items))->required(false)
-			]
-		);
-
-		return Expect::arrayOf($sections)->required(false);
-	}
-
-	public function loadConfiguration() {
-		$config = $this->config;
+	public function loadConfiguration(): void {
 		$builder = $this->getContainerBuilder();
 
 		$builder->addDefinition($this->prefix('manager'))
